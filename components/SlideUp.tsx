69ebc0635482a8bd292899132b3e96baea6b5afd
Andre <andrevitoreur@gmail.com>
2023-04-08 11:44:30 +0100

    Fixing bugs shown by Eslint

--- components/SlideUp.tsx
+++ components/SlideUp.tsx
@@ -1,33 +1,39 @@
-"use client" // this is a client component
-
 import React, { useEffect, useRef, ReactNode } from "react"
+
 interface Props {
   offset?: string
   children?: ReactNode
-  // any props that come into the component
 }
 
 export default function SlideUp({ children, offset = "0px" }: Props) {
-  const ref = useRef(null)
+  const ref = useRef<HTMLDivElement>(null)
 
   useEffect(() => {
     const observer = new IntersectionObserver(
       (entries) => {
         entries.forEach((entry) => {
           if (entry.isIntersecting) {
-            entry.target.classList.remove("opacity-0")
-            entry.target.classList.add("animate-slideUpCubiBezier")
+            const target = entry.target;
+            target.classList.remove("opacity-0")
+            target.classList.add("animate-slideUpCubiBezier")
           }
         })
       },
       { rootMargin: offset }
     )
-
-    if (ref.current) {
-      observer.observe(ref.current)
+  
+    const node = ref.current;
+    if (node) {
+      observer.observe(node);
     }
-  }, [ref])
-
+  
+    return () => {
+      if (node) {
+        observer.unobserve(node);
+      }
+    }
+  }, [ref, offset])
+  
   return (
     <div ref={ref} className="relative opacity-0">
       {children}
