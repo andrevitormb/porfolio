73d403688cca4c0c3c5777583fb14a9af90866aa
Andre <andrevitoreur@gmail.com>
2023-04-10 12:22:28 +0100

    fiexed description project myflix

--- components/ProjectsSection.tsx
+++ components/ProjectsSection.tsx
@@ -13,7 +13,7 @@ const projects = [
       "Building a Fullstack Netflix Clone with React, NextJS, TailwindCSS & Prisma ",
     features: ["Environment, Typescript, NextJS Setup", "MongoDB & Prisma connect, Database creation", "Authentication with NextAuth, Google & Github Login" , 
     "Full responsiveness on all pages" , "Cookie based authentication",
-     "API and Controllers creation" , "text-neutral-900  dark:text-neutral-100","React SWR data fetching", "Zustand state management"],
+     "API and Controllers creation" , "Detail-oriented effects and animations using TailwindCSS","React SWR data fetching", "Zustand state management"],
     image: "/myflix.png",
     github: "https://github.com/andrevitormb/myflix",
     link: "https://myflix.andremacedo.pt/",
